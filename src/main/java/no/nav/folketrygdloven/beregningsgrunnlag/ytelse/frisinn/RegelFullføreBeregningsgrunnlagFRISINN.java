4840ebcfbbd9c6fdabadb6302270dc1528603d61
Petter Ekern <petter.ekern@nav.no>
2020-04-17 10:58:09 +0200

    FIX: Fikser grenseverdi for FRISINN

--- src/main/java/no/nav/folketrygdloven/beregningsgrunnlag/ytelse/frisinn/RegelFullføreBeregningsgrunnlagFRISINN.java
+++ src/main/java/no/nav/folketrygdloven/beregningsgrunnlag/ytelse/frisinn/RegelFullføreBeregningsgrunnlagFRISINN.java
@@ -29,9 +29,6 @@ public class RegelFullføreBeregningsgrunnlagFRISINN extends DynamicRuleService<
         Specification<BeregningsgrunnlagPeriode> fastsettIkkeSøktForTilNull = rs.beregningsRegel(FastsettIkkeSøktForTil0.ID, FastsettIkkeSøktForTil0.BESKRIVELSE,
                 new FastsettIkkeSøktForTil0(), fastsettForFrilans);
 
-        Specification<BeregningsgrunnlagPeriode> fastsettBeregningsgrunnlag = rs.beregningsRegel(RegelFinnGrenseverdiFRISINN.ID, RegelFinnGrenseverdiFRISINN.BESKRIVELSE,
-                new RegelFinnGrenseverdiFRISINN(regelmodell).getSpecification(), fastsettIkkeSøktForTilNull);
-
-        return fastsettBeregningsgrunnlag;
+        return fastsettIkkeSøktForTilNull;
     }
 }
