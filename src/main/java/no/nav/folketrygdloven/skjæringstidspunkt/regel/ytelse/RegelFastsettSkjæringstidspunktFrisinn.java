57220d565bc0f5a206404e710491f5a269ac459b
Espen Johansen Velsvik <espen.velsvik@nav.no>
2020-04-20 18:06:12 +0200

    NOJIRA Flytter stp for frisinn tilbake til første dag uten ytelse

--- src/main/java/no/nav/folketrygdloven/skjæringstidspunkt/regel/ytelse/RegelFastsettSkjæringstidspunktFrisinn.java
+++ src/main/java/no/nav/folketrygdloven/skjæringstidspunkt/regel/ytelse/RegelFastsettSkjæringstidspunktFrisinn.java
@@ -1,26 +1,24 @@
 package no.nav.folketrygdloven.skjæringstidspunkt.regel.ytelse;
 
-import no.nav.folketrygdloven.skjæringstidspunkt.regel.FastsettSkjæringstidspunktLikOpptjening;
-import no.nav.folketrygdloven.skjæringstidspunkt.regelmodell.AktivitetStatusModell;
+import no.nav.folketrygdloven.skjæringstidspunkt.regelmodell.AktivitetStatusModellFRISINN;
 import no.nav.fpsak.nare.RuleService;
-import no.nav.fpsak.nare.Ruleset;
 import no.nav.fpsak.nare.doc.RuleDocumentation;
 import no.nav.fpsak.nare.evaluation.Evaluation;
 import no.nav.fpsak.nare.specification.Specification;
 
 @RuleDocumentation(value = RegelFastsettSkjæringstidspunktFrisinn.ID, specificationReference = "https://confluence.adeo.no/display/SIF/30.+Beregningsgrunnlag")
-public class RegelFastsettSkjæringstidspunktFrisinn implements RuleService<AktivitetStatusModell> {
+public class RegelFastsettSkjæringstidspunktFrisinn implements RuleService<AktivitetStatusModellFRISINN> {
 
     static final String ID = "FRISINN 1";
 
     @Override
-    public Evaluation evaluer(AktivitetStatusModell regelmodell) {
+    public Evaluation evaluer(AktivitetStatusModellFRISINN regelmodell) {
         return getSpecification().evaluate(regelmodell);
     }
 
     @SuppressWarnings("unchecked")
     @Override
-    public Specification<AktivitetStatusModell> getSpecification() {
-        return new FastsettSkjæringstidspunktLikOpptjening();
+    public Specification<AktivitetStatusModellFRISINN> getSpecification() {
+        return new FastsettSkjæringstidspunktFørPeriodeMedYtelse();
     }
 }
--- src/main/java/no/nav/folketrygdloven/skjæringstidspunkt/status/frisinn/FastsettStatusOgAndelPrPeriodeFRISINN.java
+++ src/main/java/no/nav/folketrygdloven/skjæringstidspunkt/status/frisinn/FastsettStatusOgAndelPrPeriodeFRISINN.java
@@ -13,6 +13,7 @@ import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.Periode;
 import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.grunnlag.inntekt.Arbeidsforhold;
 import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.grunnlag.inntekt.Inntektsgrunnlag;
 import no.nav.folketrygdloven.skjæringstidspunkt.regelmodell.AktivPeriode;
+import no.nav.folketrygdloven.skjæringstidspunkt.regelmodell.AktivitetStatusModell;
 import no.nav.folketrygdloven.skjæringstidspunkt.regelmodell.AktivitetStatusModellFRISINN;
 import no.nav.folketrygdloven.skjæringstidspunkt.regelmodell.BeregningsgrunnlagPrStatus;
 import no.nav.fpsak.nare.doc.RuleDocumentation;
@@ -20,7 +21,7 @@ import no.nav.fpsak.nare.evaluation.Evaluation;
 import no.nav.fpsak.nare.specification.LeafSpecification;
 
 @RuleDocumentation(FastsettStatusOgAndelPrPeriodeFRISINN.ID)
-public class FastsettStatusOgAndelPrPeriodeFRISINN extends LeafSpecification<AktivitetStatusModellFRISINN> {
+public class FastsettStatusOgAndelPrPeriodeFRISINN extends LeafSpecification<AktivitetStatusModell> {
 
     static final String ID = "FP_BR_19_2";
     static final String BESKRIVELSE = "Fastsett status per andel og periode";
@@ -30,12 +31,9 @@ public class FastsettStatusOgAndelPrPeriodeFRISINN extends LeafSpecification<Akt
     }
 
     @Override
-    public Evaluation evaluate(AktivitetStatusModellFRISINN regelmodell) {
+    public Evaluation evaluate(AktivitetStatusModell regelmodell) {
         Map<String, Object> resultater = new HashMap<>();
-        List<Periode> beregningsperioden = finnBeregningsperioder(regelmodell, resultater);
-        regelmodell.setBeregningsperioder(beregningsperioden);
-        beregningsperioden.forEach(bp -> resultater.put("Periode " + bp.getFom() + " - " + bp.getTom(), "Beregningsperiode"));
-        opprettAktivitetStatuserForAllePerioder(beregningsperioden, regelmodell);
+        opprettAktivitetStatuser(regelmodell);
         regelmodell.getAktivitetStatuser()
             .forEach(as -> resultater.put("Aktivitetstatus." + as.name(), as.getBeskrivelse()));
         regelmodell.getBeregningsgrunnlagPrStatusListe()
@@ -43,38 +41,20 @@ public class FastsettStatusOgAndelPrPeriodeFRISINN extends LeafSpecification<Akt
         return beregnet(resultater);
     }
 
-    private void opprettAktivitetStatuserForAllePerioder(List<Periode> beregningsperioden, AktivitetStatusModellFRISINN regelmodell) {
+    private void opprettAktivitetStatuser(AktivitetStatusModell regelmodell) {
+        LocalDate skjæringtidspktForBeregning = regelmodell.getSkjæringstidspunktForBeregning();
         List<AktivPeriode> aktivePerioder = regelmodell.getAktivePerioder();
-        List<AktivPeriode> aktivePerioderVedStp = hentAktivePerioderIListeMedPerioder(beregningsperioden, aktivePerioder);
+        List<AktivPeriode> aktivePerioderVedStp = hentAktivePerioderVedSkjæringtidspunkt(skjæringtidspktForBeregning, aktivePerioder);
         for (AktivPeriode ap : aktivePerioderVedStp) {
             AktivitetStatus aktivitetStatus = mapAktivitetTilStatus(ap.getAktivitet());
             regelmodell.leggTilAktivitetStatus(aktivitetStatus);
             var arbeidsforhold = AktivitetStatus.ATFL.equals(aktivitetStatus) ? ap.getArbeidsforhold() : null;
             regelmodell.leggTilBeregningsgrunnlagPrStatus(new BeregningsgrunnlagPrStatus(aktivitetStatus, arbeidsforhold));
         }
-        if (harIkkeOpprettetFrilans(regelmodell)) {
-            regelmodell.leggTilBeregningsgrunnlagPrStatus(new BeregningsgrunnlagPrStatus(AktivitetStatus.ATFL, Arbeidsforhold.frilansArbeidsforhold()));
-        }
-        if (harIkkeOpprettetNæring(regelmodell)) {
-            regelmodell.leggTilBeregningsgrunnlagPrStatus(new BeregningsgrunnlagPrStatus(AktivitetStatus.SN));
-        }
     }
 
-    private boolean harIkkeOpprettetFrilans(AktivitetStatusModellFRISINN regelmodell) {
-        return regelmodell.getBeregningsgrunnlagPrStatusListe().stream()
-            .noneMatch(st -> st.getAktivitetStatus().equals(AktivitetStatus.ATFL)
-                && st.getArbeidsforholdList().stream().anyMatch(arb -> arb.getAktivitet().equals(Aktivitet.FRILANSINNTEKT)));
-    }
-
-    private boolean harIkkeOpprettetNæring(AktivitetStatusModellFRISINN regelmodell) {
-        return regelmodell.getBeregningsgrunnlagPrStatusListe().stream()
-            .noneMatch(st -> st.getAktivitetStatus().equals(AktivitetStatus.SN));
-    }
-
-    private List<Periode> finnBeregningsperioder(AktivitetStatusModellFRISINN regelmodell, Map<String, Object> resultater) {
-        Inntektsgrunnlag inntektsgrunnlag = regelmodell.getInntektsgrunnlag();
-        LocalDate skjæringstidspunktForBeregning = regelmodell.getSkjæringstidspunktForBeregning();
-        return FinnPerioderUtenYtelse.finnPerioder(inntektsgrunnlag, skjæringstidspunktForBeregning, resultater);
+    private List<AktivPeriode> hentAktivePerioderVedSkjæringtidspunkt(LocalDate dato, List<AktivPeriode> aktivePerioder) {
+        return aktivePerioder.stream().filter(ap -> ap.inneholder(dato.minusDays(1))).collect(Collectors.toList());
     }
 
     private AktivitetStatus mapAktivitetTilStatus(Aktivitet aktivitet) {
@@ -88,12 +68,6 @@ public class FastsettStatusOgAndelPrPeriodeFRISINN extends LeafSpecification<Akt
             aktivitetStatus = AktivitetStatus.UDEFINERT;
         }
         return aktivitetStatus;
-    }
-
-    private List<AktivPeriode> hentAktivePerioderIListeMedPerioder(List<Periode> beregningsperioder, List<AktivPeriode> aktivePerioder) {
-        return aktivePerioder.stream()
-            .filter(ap -> beregningsperioder.stream().anyMatch(p -> ap.getPeriode().overlapper(p)))
-            .collect(Collectors.toList());
     }
 
 }
