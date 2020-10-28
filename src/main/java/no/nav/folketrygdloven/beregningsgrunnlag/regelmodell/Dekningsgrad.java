e56416db485bdc75de91837da34e5606fbeaa702
Petter Ekern <43809623+pekern@users.noreply.github.com>
2020-10-28 13:09:52 +0100

    TSF-1370: Legger dekningsgrad på periodenivå (#151)

--- src/main/java/no/nav/folketrygdloven/beregningsgrunnlag/regelmodell/Dekningsgrad.java
+++ src/main/java/no/nav/folketrygdloven/beregningsgrunnlag/regelmodell/Dekningsgrad.java
@@ -3,6 +3,7 @@ package no.nav.folketrygdloven.beregningsgrunnlag.regelmodell;
 import java.math.BigDecimal;
 
 public enum Dekningsgrad {
+	DEKNINGSGRAD_60(new BigDecimal("0.60")),
     DEKNINGSGRAD_65(new BigDecimal("0.65")),
     DEKNINGSGRAD_80(new BigDecimal("0.8")),
     DEKNINGSGRAD_100(BigDecimal.ONE);
