9058fc70bdc64cb8ead9d732d2f51fbb2b2fae99
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-05-25 14:27:13 +0200

    Setter beregningstidspunkt til skjæringstidspunktet dersom 8-47 B er valgt

--- src/main/java/no/nav/folketrygdloven/skjæringstidspunkt/regelmodell/AktivitetStatusModellK9.java
+++ src/main/java/no/nav/folketrygdloven/skjæringstidspunkt/regelmodell/AktivitetStatusModellK9.java
@@ -1,5 +1,7 @@
 package no.nav.folketrygdloven.skjæringstidspunkt.regelmodell;
 
+import java.time.LocalDate;
+
 import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.MidlertidigInaktivType;
 import no.nav.fpsak.nare.doc.RuleDocumentationGrunnlag;
 
@@ -13,10 +15,19 @@ public class AktivitetStatusModellK9 extends AktivitetStatusModell {
 	}
 
 	public AktivitetStatusModellK9(MidlertidigInaktivType midlertidigInaktivType,
-	                                    AktivitetStatusModell aktivitetStatusModell) {
+	                               AktivitetStatusModell aktivitetStatusModell) {
 		super(aktivitetStatusModell);
 		this.midlertidigInaktivType = midlertidigInaktivType;
 
+	}
+
+
+	@Override
+	public LocalDate getBeregningstidspunkt() {
+		if (MidlertidigInaktivType.B.equals(midlertidigInaktivType)) {
+			return getSkjæringstidspunktForBeregning();
+		}
+		return getSkjæringstidspunktForBeregning().minusDays(1);
 	}
 
 	public MidlertidigInaktivType getMidlertidigInaktivType() {
