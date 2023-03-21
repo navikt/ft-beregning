c9fa8fdfc0adf4e57adfd5248de23e4d3eec9254
Petter Ekern <petter.ekern@nav.no>
2023-03-21 16:27:44 +0100

    Sonar småtteri

--- src/main/java/no/nav/folketrygdloven/beregningsgrunnlag/BevegeligeHelligdagerUtil.java
+++ src/main/java/no/nav/folketrygdloven/beregningsgrunnlag/BevegeligeHelligdagerUtil.java
@@ -4,7 +4,6 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BevegeligeHelligdagerUtil {
 
