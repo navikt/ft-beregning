9b853178fbe606dd882af9aad08cb36c1c10fdaa
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-08-11 11:58:45 +0200

    Endrer til UUID referanse

--- kontrakt/src/main/java/no/nav/folketrygdloven/kalkulus/response/v1/beregningsgrunnlag/brev/FaktaAvklaringerForArbeid.java
+++ kontrakt/src/main/java/no/nav/folketrygdloven/kalkulus/response/v1/beregningsgrunnlag/brev/FaktaAvklaringerForArbeid.java
@@ -2,6 +2,8 @@ package no.nav.folketrygdloven.kalkulus.response.v1.beregningsgrunnlag.brev;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 
+import java.util.UUID;
+
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -9,7 +11,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import no.nav.folketrygdloven.kalkulus.felles.v1.InternArbeidsforholdRefDto;
 import no.nav.folketrygdloven.kalkulus.response.v1.Arbeidsgiver;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -17,53 +18,53 @@ import no.nav.folketrygdloven.kalkulus.response.v1.Arbeidsgiver;
 @JsonAutoDetect(fieldVisibility = NONE, getterVisibility = NONE, setterVisibility = NONE, isGetterVisibility = NONE, creatorVisibility = NONE)
 public class FaktaAvklaringerForArbeid {
 
-    @JsonProperty(value = "arbeidsgiver")
-    @NotNull
-    @Valid
-    private Arbeidsgiver arbeidsgiver;
+	@JsonProperty(value = "arbeidsgiver")
+	@NotNull
+	@Valid
+	private Arbeidsgiver arbeidsgiver;
 
-    @JsonProperty(value = "arbeidsforholdRef")
-    @Valid
-    private InternArbeidsforholdRefDto arbeidsforholdRef;
+	@JsonProperty(value = "arbeidsforholdRef")
+	@Valid
+	private UUID arbeidsforholdRef;
 
-    @JsonProperty(value = "erTidsbegrenset")
-    @Valid
-    private Boolean erTidsbegrenset;
+	@JsonProperty(value = "erTidsbegrenset")
+	@Valid
+	private Boolean erTidsbegrenset;
 
-    @JsonProperty(value = "harMottattYtelseForArbeidsforhold")
-    @Valid
-    private Boolean harMottattYtelseForArbeidsforhold;
+	@JsonProperty(value = "harMottattYtelseForArbeidsforhold")
+	@Valid
+	private Boolean harMottattYtelseForArbeidsforhold;
 
-    @JsonProperty(value = "harLønnsendringIBeregningsperioden")
-    @Valid
-    private Boolean harLønnsendringIBeregningsperioden;
+	@JsonProperty(value = "harLønnsendringIBeregningsperioden")
+	@Valid
+	private Boolean harLønnsendringIBeregningsperioden;
 
-    protected FaktaAvklaringerForArbeid() {
-    }
+	protected FaktaAvklaringerForArbeid() {
+	}
 
-    public FaktaAvklaringerForArbeid(@NotNull @Valid Arbeidsgiver arbeidsgiver,
-                                     @Valid InternArbeidsforholdRefDto arbeidsforholdRef,
-                                     @Valid Boolean erTidsbegrenset,
-                                     @Valid Boolean harMottattYtelseForArbeidsforhold,
-                                     @Valid Boolean harLønnsendringIBeregningsperioden) {
-        this.arbeidsgiver = arbeidsgiver;
-        this.arbeidsforholdRef = arbeidsforholdRef;
-        this.erTidsbegrenset = erTidsbegrenset;
-        this.harMottattYtelseForArbeidsforhold = harMottattYtelseForArbeidsforhold;
-        this.harLønnsendringIBeregningsperioden = harLønnsendringIBeregningsperioden;
-    }
+	public FaktaAvklaringerForArbeid(@NotNull @Valid Arbeidsgiver arbeidsgiver,
+	                                 @Valid UUID arbeidsforholdRef,
+	                                 @Valid Boolean erTidsbegrenset,
+	                                 @Valid Boolean harMottattYtelseForArbeidsforhold,
+	                                 @Valid Boolean harLønnsendringIBeregningsperioden) {
+		this.arbeidsgiver = arbeidsgiver;
+		this.arbeidsforholdRef = arbeidsforholdRef;
+		this.erTidsbegrenset = erTidsbegrenset;
+		this.harMottattYtelseForArbeidsforhold = harMottattYtelseForArbeidsforhold;
+		this.harLønnsendringIBeregningsperioden = harLønnsendringIBeregningsperioden;
+	}
 
-    public Arbeidsgiver getArbeidsgiver() {
-        return arbeidsgiver;
-    }
+	public Arbeidsgiver getArbeidsgiver() {
+		return arbeidsgiver;
+	}
 
-    public InternArbeidsforholdRefDto getArbeidsforholdRef() {
-        return arbeidsforholdRef;
-    }
+	public UUID getArbeidsforholdRef() {
+		return arbeidsforholdRef;
+	}
 
-    public Boolean getErTidsbegrenset() {
-        return erTidsbegrenset;
-    }
+	public Boolean getErTidsbegrenset() {
+		return erTidsbegrenset;
+	}
 
 
 	public Boolean getHarMottattYtelseForArbeidsforhold() {
@@ -71,6 +72,6 @@ public class FaktaAvklaringerForArbeid {
 	}
 
 	public Boolean getHarLønnsendringIBeregningsperioden() {
-        return harLønnsendringIBeregningsperioden;
-    }
+		return harLønnsendringIBeregningsperioden;
+	}
 }
