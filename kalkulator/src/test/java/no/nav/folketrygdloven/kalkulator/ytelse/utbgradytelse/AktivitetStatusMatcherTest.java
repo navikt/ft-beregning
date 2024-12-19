cbf1d675408a6b19fccab4d9cc367f01da56b755
inga <inga.kristine.holen@nav.no>
2024-12-19 12:56:49 +0100

    TSFF-1006: Nye UttakArbeidTyper for at aktivitet skal kunne kompenseres full ut når den faller bort

--- kalkulator/src/test/java/no/nav/folketrygdloven/kalkulator/ytelse/utbgradytelse/AktivitetStatusMatcherTest.java
+++ kalkulator/src/test/java/no/nav/folketrygdloven/kalkulator/ytelse/utbgradytelse/AktivitetStatusMatcherTest.java
@@ -11,14 +11,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class AktivitetStatusMatcherTest {
 
 	@Test
-	void skal_mappe_SN_IKKE_AKTIV_til_SN() {
-		boolean matcher = AktivitetStatusMatcher.matcherStatus(AktivitetStatus.SELVSTENDIG_NÆRINGSDRIVENDE, UttakArbeidType.SELVSTENDIG_NÆRINGSDRIVENDE_IKKE_AKTIV);
+	public void skal_mappe_SN_IKKE_AKTIV_til_SN() {
+		boolean matcher = AktivitetStatusMatcher.matcherStatus(AktivitetStatus.SELVSTENDIG_NÆRINGSDRIVENDE, UttakArbeidType.SN_IKKE_AKTIV);
 		assertTrue(matcher, "SN_IKKE_AKTIV skulle matche sevlstendig næringsdrivende");
 	}
 
 	@Test
-	void skal_mappe_FL_IKKE_AKTIV_til_FL() {
-		boolean matcher = AktivitetStatusMatcher.matcherStatus(AktivitetStatus.FRILANSER, UttakArbeidType.FRILANSER_IKKE_AKTIV);
+	public void skal_mappe_FL_IKKE_AKTIV_til_FL() {
+		boolean matcher = AktivitetStatusMatcher.matcherStatus(AktivitetStatus.FRILANSER, UttakArbeidType.FL_IKKE_AKTIV);
 		assertTrue(matcher, "FL_IKKE_AKTIV skulle matche sevlstendig næringsdrivende");
 	}
 
