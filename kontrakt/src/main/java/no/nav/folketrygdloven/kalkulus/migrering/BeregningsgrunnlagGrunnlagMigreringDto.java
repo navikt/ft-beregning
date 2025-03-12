be97edd18deae5161f74d1e51fa6ad79e24b2a1f
Petter Ekern <petter.ekern@nav.no>
2025-03-12 13:04:12 +0100

    FIX: Tomme lister sendes som tomme lister istedenfor null

--- kontrakt/src/main/java/no/nav/folketrygdloven/kalkulus/migrering/BeregningsgrunnlagGrunnlagMigreringDto.java
+++ kontrakt/src/main/java/no/nav/folketrygdloven/kalkulus/migrering/BeregningsgrunnlagGrunnlagMigreringDto.java
@@ -2,11 +2,14 @@ package no.nav.folketrygdloven.kalkulus.migrering;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import no.nav.folketrygdloven.kalkulus.kodeverk.BeregningsgrunnlagTilstand;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BeregningsgrunnlagGrunnlagMigreringDto extends BaseMigreringDto {
     @Valid
     private BeregningsgrunnlagMigreringDto beregningsgrunnlag;
