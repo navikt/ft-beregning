820e12b0469badba24269bea8d7aaecf76cb8964
anjaaalerud <anja.gotesen.aalerud@nav.no>
2025-05-08 13:34:35 +0200

    TFP-6238 unused import fjernet

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/kontrollerfakta/utledere/ArbeidUnderAAPTilfelleUtleder.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/steg/kontrollerfakta/utledere/ArbeidUnderAAPTilfelleUtleder.java
@@ -1,12 +1,11 @@
 package no.nav.folketrygdloven.kalkulator.steg.kontrollerfakta.utledere;
 
+import java.util.Optional;
+
 import no.nav.folketrygdloven.kalkulator.input.FaktaOmBeregningInput;
 import no.nav.folketrygdloven.kalkulator.modell.beregningsgrunnlag.BeregningsgrunnlagGrunnlagDto;
 import no.nav.folketrygdloven.kalkulator.steg.kontrollerfakta.ArbeidUnderAAPTjeneste;
-import no.nav.folketrygdloven.kalkulator.steg.kontrollerfakta.EtterlønnSluttpakkeTjeneste;
 import no.nav.folketrygdloven.kalkulus.kodeverk.FaktaOmBeregningTilfelle;
-
-import java.util.Optional;
 
 
 public class ArbeidUnderAAPTilfelleUtleder implements TilfelleUtleder {
