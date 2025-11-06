2af68d0c8ae7fa974440e222c8e5053177857681
Petter Ekern <petter.ekern@nav.no>
2025-11-06 06:41:47 +0100

    TFP-6448: Lager periodesplitt hvis gradering startert i en periode der totalt refusjonskrav overstiger brutto og graderingsandelen ikke har refusjonskrav

---
+++ beregningsregler/src/main/java/no/nav/folketrygdloven/beregningsgrunnlag/perioder/gradering/ErTotaltRefusjonskravStørreEnnBrutto.java
@@ -0,0 +1,41 @@
+package no.nav.folketrygdloven.beregningsgrunnlag.perioder.gradering;
+
+import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.periodisering.BruttoBeregningsgrunnlag;
+import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.periodisering.PeriodisertBruttoBeregningsgrunnlag;
+import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.periodisering.gradering.PeriodeModellGradering;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+class ErTotaltRefusjonskravStørreEnnBrutto {
+
+    private ErTotaltRefusjonskravStørreEnnBrutto() {
+        // skjul public constructor
+    }
+
+    public static boolean vurder(List<PeriodisertBruttoBeregningsgrunnlag> periodisertBeregningsgrunnlag, LocalDate dato) {
+        var totaltRefusjonskravPrÅr = beregnTotaltRefusjonskravPrÅrPåDato(periodisertBeregningsgrunnlag, dato);
+        var totalBruttoPrÅr = beregnTotalBruttoPrÅr(periodisertBeregningsgrunnlag, dato);
+        return totaltRefusjonskravPrÅr.compareTo(totalBruttoPrÅr) >= 0;
+    }
+
+    private static BigDecimal beregnTotalBruttoPrÅr(List<PeriodisertBruttoBeregningsgrunnlag> grunnlag, LocalDate dato) {
+        return grunnlag.stream()
+            .filter(p -> p.getPeriode().inneholder(dato))
+            .flatMap(p -> p.getBruttoBeregningsgrunnlag().stream())
+            .map(BruttoBeregningsgrunnlag::getBruttoPrÅr)
+            .filter(Objects::nonNull)
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private static BigDecimal beregnTotaltRefusjonskravPrÅrPåDato(List<PeriodisertBruttoBeregningsgrunnlag> grunnlag, LocalDate dato) {
+	    return grunnlag.stream()
+		        .filter(p -> p.getPeriode().inneholder(dato))
+                .flatMap(p -> p.getBruttoBeregningsgrunnlag().stream())
+		        .map(BruttoBeregningsgrunnlag::getRefusjonPrÅr)
+		        .filter(Objects::nonNull)
+		        .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
