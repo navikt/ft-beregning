167538b49b8a914ed0651cd0e8f4c893d52a47ed
Petter Ekern <petter.ekern@nav.no>
2025-11-25 07:10:51 +0100

    Chore: Fjerner valideringer på interne dto'er som aldri blir sjekket

--- kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/avklaringsbehov/dto/FastsettBGTidsbegrensetArbeidsforholdDto.java
+++ kalkulator/src/main/java/no/nav/folketrygdloven/kalkulator/avklaringsbehov/dto/FastsettBGTidsbegrensetArbeidsforholdDto.java
@@ -3,22 +3,10 @@ package no.nav.folketrygdloven.kalkulator.avklaringsbehov.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
-
 public class FastsettBGTidsbegrensetArbeidsforholdDto {
 
-
-    @Valid
-    @Size(max = 100)
     private List<FastsattePerioderTidsbegrensetDto> fastsatteTidsbegrensedePerioder;
-
-    @Min(0)
-    @Max(178956970)
     private Integer frilansInntekt;
-
 
     FastsettBGTidsbegrensetArbeidsforholdDto() {
         // For Jackson
